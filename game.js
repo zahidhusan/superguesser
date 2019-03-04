7a56536502e0daff3c7cbb790105879e8c8338ea
zahid hussain <zahidrafiq004@gmail.com>
2019-03-04 15:54:09 +0530

    commitall2

--- game.js
+++ game.js
@@ -3,24 +3,63 @@
 		   var person;
 							function fun(e)
 											{
-												count++;
+												  count++;
+							                      document.getElementById("attempt").style.visibility="visible";
+												
+												document.getElementById("attempt").innerHTML=" Guesses Attempted : "+ count;
+												 
 											var y = document.getElementById("myAudio2");  
 										    y.play();
-										    alert("Turn number : "+count+ " You guessed :  "+e.value);
+											 
+												 
+										    
 											var  a = Math.floor(Math.random() * 10) + 1;
-											 alert("Our Number Is:  "+a) ; 
-											if(e.value == a){
-											score++;
+											 document.getElementById("img").style.visibility="visible";
+											 //alert("Our Number Is:  "+a) ;
+									if (a==1)
+									{
+									document.getElementById('img').src='1.png';	
+									}
+									if(a==2){
+											document.getElementById('img').src='2.jpeg';	
+									}
+									if(a==3){
+											document.getElementById('img').src='3.jpeg';	
+									}
+									if(a==4){
+											document.getElementById('img').src='4.jpeg';	
+									}
+									if(a==5){
+											document.getElementById('img').src='5.jpeg';	
+									}
+									if(a==6){
+											document.getElementById('img').src='6.png';	
+									}
+									if(a==7){
+											document.getElementById('img').src='7.jpg';	
+									}
+									if(a==8){
+											document.getElementById('img').src='8.jpg';	
+									}
+									if(a==9){
+											document.getElementById('img').src='9.jpg';	
+									}
+									if(a==10){
+											document.getElementById('img').src='10.jpg';	
+									}
+											    if(e.value == a){
+											          score++;
 											var x = document.getElementById("myAudio");  
 											  x.play();
 											 var j = document.getElementById("myAudio5");  
 											  j.play(); 
-											alert(" WoW: You guessed it right");
+											//alert(" WoW: You guessed it right");
 															} 
-											   
-				   
-				   
+											// document.getElementById("sc").style.visibility="visible";   
+				                           // document.getElementById("sc").innerHTML= person +"your "+ "Score is  : "+ score;
+				                               
 				    if(count == 10){
+						
 					             var t = document.getElementById("myAudio6");  
 							           t.play();
 				              alert("Attempts Finished:  ") ;
@@ -50,8 +89,9 @@
                         //var z = document.getElementById("myAudio7");  
 			               //z.play();
 						      //z.loop = true;
-								
-							          
+					 document.getElementById("attempt").style.visibility="hidden";			
+						  document.getElementById("img").style.visibility="hidden";	
+                        //document.getElementById("sc").style.visibility="hidden";						  
 			 count = 0;
 			 score = 0;
 		   person = prompt("Please enter your name", "KH");
